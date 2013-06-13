253d47a7938555066a4a00abc712e24f7d03cbb8
Marc Halpern <marc@Marcs-Mac-mini.local>
2013-06-13 14:57:48 -0700

    Initial commit after creating project from template.

---
+++ MyFirstApp/MyFirstApp/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MyFirstApp
+//
+//  Created by Marc Halpern on 6/13/13.
+//  Copyright (c) 2013 Marc Halpern. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
